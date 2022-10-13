3acbb17dfdc01792192909ee978131051230209e
SamChandran <91770355+SamChandran@users.noreply.github.com>
2022-10-13 11:20:57 +0530

    initial commit 1

--- Main.py
+++ Main.py
@@ -1,16 +1,14 @@
 from typing import List
 
 def selectionSort(array, size) -> List[int]:
-  # Write your code here
-  for i in range(len(array)):
-    minimum = i
-    for j in range(i+1,len(array)):
-      if array[minimum]>array[j]:
-        minimum=j
-    array[i],array[minimum] = array[minimum],array[i]
+  for select in range(size):
+    check = select
+    for sort in range(select,size):
+      if(array[sort]<array[select]):
+        array[sort],array[select] = array[select],array[sort]
   return array
 
-# Do not change the following code
+# Do not change the following code//
 input_data = input()
 data = []
 for item in input_data.split(', '):
